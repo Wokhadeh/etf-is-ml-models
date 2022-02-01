06902cc9184be47c7f81a886d2897f4c30c33f54
Predrag Mitrović <61320857+Wokhadeh@users.noreply.github.com>
2022-02-01 22:47:19 +0100

    Linear regression using sklearn

--- main.py
+++ main.py
@@ -3,12 +3,15 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+
 
 # predict max amount of money that person is ready to pay for new car
 
 
 # 1. Read data and print last five rows
 pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
 pd.set_option('display.width', None)
 dataset_car_purchase = pd.read_csv('datasets/car_purchase.csv')
 print(dataset_car_purchase.tail())
@@ -36,8 +39,26 @@ plt.show()
 data_train = dataset_car_purchase[['age', 'annual_salary', 'credit_card_debt', 'net_worth']]
 labels = dataset_car_purchase['max_purchase_amount']
 
-# 6. Prediction using linear regression from sklearn
+# 5. Prediction using linear regression from sklearn
 
+lr_skl_model = LinearRegression()
+
+X_train, X_test, Y_train, Y_test = train_test_split(data_train, labels, random_state=1)
+
+lr_skl_model.fit(X_train.values.reshape(-1, 4), Y_train.values)
+predicted = lr_skl_model.predict(X_test.values.reshape(-1, 4))
+print('Model: ',end='')
+for pair in zip(lr_skl_model.coef_[:-1], data_train.columns):
+    print(str(pair[0]) + ' * ' + pair[1] + ' + ',end='')
+print(lr_skl_model.coef_[-1])
+print('Mean squared error function: ' + str((1/(2*len(predicted)))*sum(Y_test.sub(predicted)**2)))
+print('Score: ' + str(lr_skl_model.score(X_test.values.reshape(-1, 4), Y_test.values)))
+
+
+#ser_predicted = pd.Series(data=predicted, name='predicted', index=X_test.index)
+# print(ser_predicted)
+# new_df = pd.concat([Y_test, ser_predicted], axis=1)
+# print(new_df)
 
 
 
